25b3be2347083b50db2fb862918dab0768d01f79
Rob <robert.j1450@gmail.com>
2019-10-21 21:16:13 -0400

    added header brand name and google font

--- src/header.js
+++ src/header.js
@@ -3,8 +3,11 @@ import React, { Component } from "react";
 class Header extends Component {
 render(){
 return (
-    <div id="header">
-      <h1>Story Traveler</h1>
+    <div id="header" className="brand-name">
+      <h1>
+        <span className="brand-name-primary">Story</span>
+        <span className="brand-name-secondary">Traveler.</span>
+        </h1>
      </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './header';
+import "./header.css";
 
 ReactDOM.render(
   <Header/>, 
