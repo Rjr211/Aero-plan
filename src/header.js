1986ff1b554330ce3c012d6f3f7b588db8913827
Rob <robert.j1450@gmail.com>
2019-11-01 01:55:59 -0400

    added styling for nav items, underline animation, and media query animation

--- src/header.js
+++ src/header.js
@@ -4,11 +4,19 @@ class Header extends Component {
 render(){
 return (
     <div id="navBar">
-      <h1 className="brand-name">Frequent Weekender
+      <h1 className="brand-name-primary">Frequent Weekender
+        </h1>
+        <h1 className="brand-name-secondary">FW
         </h1>
             <ul>
-               <li className="nav-hotel"><a href="#">hotels</a></li>
-                <li className="nav-flights"><a href="#">flights</a></li>
+              <li className="nav-menu">
+                <a href="#" >
+                 <i class="material-icons">menu</i>
+                </a>
+              </li>
+              <li className="nav-items"><a href="#" class="nav-link">Hotels</a></li>
+              <li className="nav-items">|</li>
+              <li className="nav-items"><a href="#" class="nav-link">Flights</a></li>
             </ul>
      </div>
     );
