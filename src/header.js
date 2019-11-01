ff0c16ef1c358e64b80054c28d1e41236fec3b83
Rob <robert.j1450@gmail.com>
2019-10-31 21:31:19 -0400

    added nav items

--- src/header.js
+++ src/header.js
@@ -6,6 +6,10 @@ return (
     <div id="navBar">
       <h1 className="brand-name">Frequent Weekender
         </h1>
+            <ul>
+               <li className="nav-hotel"><a href="#">hotels</a></li>
+                <li className="nav-flights"><a href="#">flights</a></li>
+            </ul>
      </div>
     );
   }
